b6e92e202ec548c4c8f8384db5f5bd9b11cb9064
Julia Konefal <konefal.julia@gmail.com>
2022-01-28 15:46:24 +0100

    restart app on save

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include "database.h"
 #include "future.h"
@@ -6,13 +8,21 @@
 #include "past.h"
 #include "statistics.h"
 
+bool quit = false;
+
+void quit_program()
+{
+    quit = true;
+    gtk_main_quit();
+}
+
 int main(int argc, char* argv[])
 {
     gtk_init(&argc, &argv);
     GtkBuilder* ui = gtk_builder_new_from_file("main.glade");
     GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(ui, "window"));
     g_signal_connect(
-        G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+        G_OBJECT(window), "destroy", G_CALLBACK(quit_program), NULL);
 
     home_main(ui);
     past_main(ui);
@@ -22,4 +32,9 @@ int main(int argc, char* argv[])
     gtk_widget_show_all(GTK_WIDGET(window));
     gtk_main();
     g_object_unref(ui);
+
+    if (!quit) {
+        char* const args[] = { "/proc/self/exe", 0 };
+        execv(args[0], args);
+    }
 }
